1500b280792b4521565c00e3d9b8f0443207f4d4
Steph <steph_salzburg@hotmail.com>
2024-08-28 10:22:25 -0400

    added more logic to the other functions to make them return data. While this is not fully functional yet it is a step in the right direction of learning

--- Project3/Account.php
+++ Project3/Account.php
@@ -3,21 +3,42 @@ class Account{
     public $accountNumber; 
     public $accountHolder;
     public $balance; 
+    public $transactions = [];
 
     public function deposit($depositAmount){
         $this->balance += $depositAmount;
+        if(!empty($depositAmount)){
+            $this->transactions[] = $depositAmount;
+
+        }else{
+            $this->transactions[] = 0;
+        }
         
         return $this->balance;
-        
-        
-           
+            
     }
-    public function withdraw(){ 
+    public function withdraw($depositAmount){ 
+    $this ->balance -= $depositAmount;
+    return $this->balance;
     }
+
     public function getBalance(){
+        return $this ->balance;
+
     
     }
+
+    public function logTransactions($title, $amount, $newBalance)
+    {
+        $this->transactions[] = ["title"=> $title,
+        "amount"=> $amount,"newBalance" => $newBalance];
+        return $this->transactions;
+
+
+
+    }
 }
+
 
 
 ?>
